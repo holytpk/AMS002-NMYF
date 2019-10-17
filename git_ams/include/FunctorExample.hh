44bed3fd250666b6f109eaf4fd23cd792d72b7b6
Lingqiang He <37253359+holytpk@users.noreply.github.com>
2019-10-16 19:08:10 -1000

    Add files via upload

--- git_ams/include/FunctorExample.hh
+++ git_ams/include/FunctorExample.hh
@@ -9,7 +9,7 @@ class FunctorExample
 {
    public:
       FunctorExample();
-      FunctorExample(const Char_t *Name, Double_t XMin, Double_t XMax, TF1 *fyp, TF1 *fyHe);
+      FunctorExample(const Char_t *Name, Double_t XMin, Double_t XMax, TF1 *fyp, TF1 *fyHe, TH1 *J_sum);
       FunctorExample(const FunctorExample &);
       virtual ~FunctorExample();
 
@@ -21,7 +21,7 @@ class FunctorExample
       void Print();
 
       void SetProtonFlux(TF1 *flux_p) { _flux_p = flux_p; }
-      void AddElementFlux(TF1 *flux_el, Double_t A) { _flux_elem.push_back(flux_el); _A.push_back(A); }
+      void AddElementFlux(TF1 *flux_el, Double_t A) { _flux_elem.push_back(flux_el); _A.push_back(A); } 
 
    private:
       UInt_t _npars;
@@ -31,6 +31,7 @@ class FunctorExample
       TF1 *_func; //!
       TF1 *_fyp;
       TF1 *_fyHe;
+      TH1 *_J_sum; // R(P), ratio of heavier/helium
 
       TF1 *_flux_p;
       std::vector<TF1 *> _flux_elem;
@@ -89,4 +90,35 @@ fluxp->Draw(); gPad->SetLogx(); gPad->SetLogy()
 fyf->Eval(1)
 fe->SetProtonFlux(fluxp)
 fyf->Eval(1)
+
+Spline *s = new Spline("fsp", 7, Spline::LogLog | Spline::PowerLaw);
+double xx[7], yy[7];
+for (int i=0; i<7; ++i) { xx[i] = fit_Fe->GetParameter(2+i); yy[i] = fit_Fe->GetParameter(9+i); }
+s->SetXNodes(xx)
+s->SetYNodes(yy)
+s->SetSpectralIndices(fit_Fe->GetParameter(0), fit_Fe->GetParameter(1));
+s->Print()
+xx[0]
+xx[1]
+fit_Fe->Print()
+HistTools::PrintFunction(fit_Fe)
+TF1 *fflux = new TF1("fflux", "1e4*pow(x,-2.8)", 0.1, 3000)
+TF1 *fmul = HistTools::CombineTF1(fflux, fyfp, HistTools::Multiply, "fmul")
+fyfp->Draw(); gPad->SetLogx(); gPad->SetLogy()
+new TCanvas; fflux->Draw(); gPad->SetLogx(); gPad->SetLogy()
+new TCanvas; fmul->Draw(); gPad->SetLogx(); gPad->SetLogy()
+fmul->Integral(0.1, 3e3)
+fmul->Integral(0.1, 1e3)
+fmul->Integral(0.1, 1e2)
+fmul->Integral(0.1, 10)
+fyfp->Eval(1e4)
+fyfp->Eval(1e3)
+fmul->Integral(0.1, 1e4)
+fmul->Integral(0.1, 1e4)/fmul->Integral(0.1, 3e3)
+HistTools::CombineTF1Const(fmul, A/4, HistTools::MultiplyConst, "fmulconst")
+HistTools::PrintFunction(fmul)
+.q
+TF1 *fyfp = NeutronMonitors::YieldFunctions::CreateFunction("fyfp", 0.1, 3e3, NeutronMonitors::YieldFunctions::Mishev13H1, Particle::PROTON, Energy::RIGIDITY)
+fyfp->Draw()
+
 */
