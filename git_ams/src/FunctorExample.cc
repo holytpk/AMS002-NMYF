f75aa561cc8e66ee500c850b7b40f9451c41d71d
Lingqiang He <37253359+holytpk@users.noreply.github.com>
2020-07-02 00:28:38 -1000

    Add files via upload

--- git_ams/src/FunctorExample.cc
+++ git_ams/src/FunctorExample.cc
@@ -17,18 +17,18 @@ FunctorExample::FunctorExample() :
 // explicit constructor
 FunctorExample::FunctorExample(const Char_t *Name, Double_t XMin, Double_t XMax, TF1 *fyp, TF1 *fyHe) :
    _func(NULL), _fyp(fyp), _fyHe(fyHe),
-   _flux_p(NULL), _flux_He(NULL)
+   _flux_p(NULL), _flux_p_low(NULL), _flux_He(NULL) 
 {
    _nparsp = fyp->GetNpar();
    _nparsHe = fyHe->GetNpar();
    _npars = _nparsp + _nparsHe;
 
-   _func = new TF1(Name, this, XMin, XMax, _npars);
-   _func->SetNpx(1000);
+   _func = new TF1(Name, this, XMin, XMax, _npars); 
+   _func->SetNpx(500);
 
    // display R_sum
    _f_R_sum = new TF1(Form("%s_R_sum", Name), this, &FunctorExample::RSum, XMin, XMax, 0);
-   _f_R_sum->SetNpx(1000);
+   _f_R_sum->SetNpx(500);
 
    HistTools::CopyParameters(_fyp, _func); // { _fyp_par0, _fyp_par1, ..., _fyp_npars-1, 0, ... }
    HistTools::CopyParameters(_fyHe, _func, _nparsp);// { _fyp_par0, _fyp_par1, ..., _fyp_npars-1, _fyHe_par0, ... }
@@ -57,6 +57,44 @@ Double_t FunctorExample::operator()(Double_t *x, Double_t *par)
 {
    Double_t xx = x[0];
 
+   Double_t xmin, xmax;
+   _flux_p->GetRange(xmin, xmax); 
+
+   _fyp->SetParameters(par);
+   _fyHe->SetParameters(par+_nparsp);
+
+   Double_t R_sum = RSum2(x, par); // Rsum w/ the 1.746 term, Rsum2 w/o the 1.746 term. 
+
+   Double_t f;
+
+   if (xx<xmin) f = _flux_p_low->Eval(xx)*_fyp->Eval(xx); 
+   if (xx>=xmin) f = _flux_p->Eval(xx)*_fyp->Eval(xx); 
+
+   for (int i = 0; i < (int)_A.size(); ++i)
+   {
+
+	if (xx<xmin) f += _flux_elem_low[i]->Eval(xx)*_fyHe->Eval(xx)*(1.+R_sum); 
+	if (xx>=xmin) f += _flux_elem[i]->Eval(xx)*_fyHe->Eval(xx)*(1.+R_sum);
+
+        //if (f!=0 && _flux_elem[i]->Eval(xx)!=0) f += _flux_elem[i]->Eval(xx)*_fyHe->Eval(xx)*(1.+R_sum);
+   	//if (f==0 && _flux_elem[i]->Eval(xx)!=0) f += _flux_elem[i]->Eval(xx)*_fyHe->Eval(xx); 
+   	//if (f==0 && _flux_elem[i]->Eval(xx)==0) f += _flux_He->Eval(xx)*_fyHe->Eval(xx)*R_sum;
+   } 
+
+   for (int i = 0; i < (int)_A_above.size(); ++i)
+   {
+	f += _flux_above[i]->Eval(xx)*_fyHe->Eval(xx); 
+   }
+
+   return f;
+}
+
+/*
+// operator OG
+Double_t FunctorExample::operator()(Double_t *x, Double_t *par)
+{
+   Double_t xx = x[0];
+
    _fyp->SetParameters(par);
    _fyHe->SetParameters(par+_nparsp);
 
@@ -72,6 +110,7 @@ Double_t FunctorExample::operator()(Double_t *x, Double_t *par)
 
    return f;
 }
+*/ 
 
 Double_t FunctorExample::Eval(Double_t x)
 {
@@ -90,10 +129,27 @@ Double_t FunctorExample::RSum(Double_t *x, Double_t *par)
    {
       R_sum += (_A_ave[i]/4.)*(_flux_elem_ave[i]->Eval(xx)/_flux_elem_ave[1]->Eval(xx));
    }
-   R_sum = R_sum + 1.746*(_A_ave[7]/4.)*_flux_elem_ave[7]->Eval(xx)/_flux_elem_ave[1]->Eval(xx);
+   R_sum = R_sum + 1.746*(_A_ave[7]/4.)*_flux_elem_ave[7]->Eval(xx)/_flux_elem_ave[1]->Eval(xx); 
 
    return R_sum;
-}
+} 
+
+// estimated F(R,t)/<F(R,t)> ratio, replacing _ave w/ the actual ratio values 
+Double_t FunctorExample::RSum2(Double_t *x, Double_t *par)
+{
+   Double_t xx = x[0];
+
+   Double_t R_sum = 0.;
+
+   // starts from He+1
+   for (int i = 2; i < (int)_A_ave.size(); ++i)
+   {
+      R_sum += (_A_ave[i]/4.)*(_flux_elem_ave[i]->Eval(xx)/_flux_elem_ave[1]->Eval(xx)); 
+   }
+   // R_sum = R_sum + 1.746*(_A_ave[7]/4.)*_flux_elem_ave[7]->Eval(xx)/_flux_elem_ave[1]->Eval(xx); 
+
+   return R_sum;
+} 
 
 void FunctorExample::Print()
 {
